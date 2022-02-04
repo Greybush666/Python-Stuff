337721b94e2bfe5cf40f7498c223d28593440b46
Greybush666 <97858209+Greybush666@users.noreply.github.com>
2022-02-03 23:11:28 -0600

    Add files via upload

---
+++ PizzaOrder.py
@@ -0,0 +1,12 @@
+print("Welcome to NDP!")
+size = input("What size pizza do you want S, M, or L ")
+add_pep = input("Do you want pep? y or n ")
+extra_cheese = input("Do you want extra cheese y or n ")
+
+S = 15
+M = 20
+L = 25                     
+
+if size == s:
+    
+    print(f"Your final bill is ${S}")                     
