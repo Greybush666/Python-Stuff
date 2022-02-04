570c91d4397af6e2c7bd344c5e5b4c901926f2a4
Greybush666 <97858209+Greybush666@users.noreply.github.com>
2022-02-03 20:30:43 -0600

    Add files via upload

---
+++ IfElse.py
@@ -0,0 +1,16 @@
+print("Welcome to the FUCK ZONE")
+
+height = int(input("What is your height in CM? "))
+
+if height >= 120:
+    print("You can ride the fuck")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5")
+    elif age <= 18:
+        print("Pleast pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Fuck you, no")
+    
